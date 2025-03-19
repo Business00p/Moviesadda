e977d749de1c91638b58131f33ce7d4c46694099
Tv show <businesskaro0565@gmail.com>
2025-03-19 18:50:24 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -919,7 +919,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         reply_markup = InlineKeyboardMarkup(buttons)
     
         await query.message.edit_text(
-            caption=script.HELP_TXT,
+            text=script.HELP_TXT,
 	    reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML 
 	)
